79fb82384903b545e8aa3ed1e7305f6cc1ec56f2
now4est <now4est@pop-os.localdomain>
2023-12-17 15:44:02 +0900

    Add: 5分前にlaunch

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from calendar_helper import authenticate_and_get_service, save_today_events_to_csv
-import browser_handler
-
 import schedule
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
+import zoneinfo
 import argparse
 import csv
 from pprint import pprint
 
-def get_meeting():
+from calendar_helper import authenticate_and_get_service, save_today_events_to_csv
+import browser_handler
+
+def get_meeting(service):
     """API を叩いてイベント情報を取ってくる
     """
     # Google カレンダーのイベントを取得する
-    service = authenticate_and_get_service()
     save_today_events_to_csv(service)
     print('Today\'s events have been saved to events.csv.')
     # 一度すべての job を消す
@@ -29,6 +29,10 @@ def launch_app(info):
     """イベント日時が迫ったらミーティングアプリを立ち上げる
     """
     # open する
+    if info['URL Type'] == 'Zoom':
+        browser_handler.save_credentials('Zoom')
+    if len(info['URL']) == 0:
+        return
     browser_handler.browser_handler(info['URL'], info['URL Type'])
     # job は定期実行として登録されているので残りをキャンセル
     schedule.clear()
@@ -36,24 +40,32 @@ def launch_app(info):
 def add_job(info):
     """イベント日時で job の登録
     """
-    start_time = info['dt_start_time'].strftime('%H:%M')
-    schedule.every().day.at(start_time).do(launch_app, info)
+    info['dt_start_time']
+    now = datetime.now(zoneinfo.ZoneInfo('Asia/Tokyo'))
+    start = info['dt_start_time']
+    start_b5m = start - timedelta(minutes=5)
+    if start_b5m > now:
+        job_time = start_b5m.strftime('%H:%M')
+    elif start > now:
+        job_time = start.strftime('%H:%M')
+    else:
+        return
+    schedule.every().day.at(job_time).do(launch_app, info)
 
 def main():
-    browser_handler.save_credentials('Zoom')
     parser = argparse.ArgumentParser(
             prog='自動化アプリ (仮)',
             description='面倒な作業を自動化するアプリです')
-    parser.add_argument('during', metavar='N', type=int, nargs=1, help='実行間隔')
+    parser.add_argument('during', metavar='N', type=int, nargs='?', default=5, help='実行間隔')
     args = parser.parse_args()
-    DURING = 5
-    if 1 <= args.during[0]:
-        DURING = args.during[0]
+    if 1 <= args.during:
+        DURING = args.during
 
     # 初回
-    get_meeting()
+    service = authenticate_and_get_service()
+    get_meeting(service)
     # N分間隔で実行
-    schedule.every(DURING).minutes.do(get_meeting)
+    schedule.every(DURING).minutes.do(get_meeting, service)
 
     # 無限ループ
     while True:
