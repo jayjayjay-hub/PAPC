b7b5162401182d4fe30d1e36cc9167738411dfcf
Jay Takahashi <qsakura14555@gmail.com>
2023-12-17 12:20:58 +0900

    not_front

--- calendar_helper.py
+++ calendar_helper.py
@@ -4,12 +4,10 @@ from googleapiclient.discovery import build
 import re
 import webbrowser
 import time
+import csv
 
 # Google Calendar APIのスコープ
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
-
-# 開いた会議のURLを保存するセット
-opened_meetings = set()
 
 def authenticate_and_get_service():
 	# Google Calendar APIの認証とサービスの取得
@@ -18,46 +16,64 @@ def authenticate_and_get_service():
 	return build("calendar", "v3", credentials=creds)
 
 def find_next_meeting_url(service):
-	while True:
-	# 現在の日本時間
-		now_jp = datetime.now(timezone(timedelta(hours=9)))
-		print(f"Checking for meetings at {now_jp}")
+	# 今日のすべての予定を取得
+	events = get_today_events(service)
+	write_csv(events, "events.csv")
 
-		# 5分おきに探す
-		time_to_wait = 5 * 60
-		time.sleep(time_to_wait)
+def write_csv(events, csv_filename):
+    # CSVファイルに予定を書き込む
+    with open(csv_filename, 'w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(["Start Time", "URL", "Summary"])
+        # 予定をCSVに書き込む
+        for event in events:
+            summary = event.get("summary", "No Title")
+            start_time = event["start"].get("dateTime", event["start"].get("date"))
+            # Google Meetの場合はhangoutLinkを使用し、それ以外はdescriptionからURLを抽出
+            if "hangoutLink" in event:
+                URL = event["hangoutLink"]
+            else:
+                description = event.get("description", "")
+                URL_match = re.search(r"<a\s+href=\"(?P<url>https?://[^\"]+)\">", description)
+                URL = URL_match.group("url") if URL_match else "No URL"
 
-		# 今日のすべての予定を取得
-		events = get_today_events(service)
+            # URLがあればCSVに書き込む
+            if URL != "No URL":
+                output = [start_time, URL, summary]
+                # URLの種類を判定
+                URL_type = classify_url_type(URL)
+                output = [start_time, URL, summary, URL_type]
+                writer.writerow(output)
 
-		for event in events:
-			# 開始時刻が現在時刻より遅く、かつ、現在時刻との差が8分以内の場合
-			if event["start"]["dateTime"] > now_jp and (now_jp - event["start"]["dateTime"]).total_seconds() < 480:
-				meeting_url = get_meeting_url(event)
-				if meeting_url and meeting_url not in opened_meetings:
-					opened_meetings.add(meeting_url)
-					open_meeting_url_in_browser(meeting_url)
-					return meeting_url
+def classify_url_type(url):
+    # URLがZoomのものか判定
+    if re.search(r"zoom\.us", url):
+        return "zoom"
+    # URLがGoogle Meetのものか判定
+    elif re.search(r"meet\.google\.com", url):
+        return "meet"
+    else:
+        return "else"
 
 def get_today_events(service):
-	# 今日の開始と終了の日時を設定
-	start_time = datetime.today()
-	end_time = start_time + timedelta(days=1)
-
+    # 今日の開始と終了の日時を設定
+    now = datetime.utcnow().isoformat() + "Z"
+    print(now)
+    end_time = (datetime.utcnow() + timedelta(days=1)).isoformat() + "Z"
 	# 今日のすべての予定を取得
-	events_result = (
+    events_result = (
 		service.events()
 		.list(
 			calendarId="primary",
-			timeMin=start_time.isoformat(),
-			timeMax=end_time.isoformat(),
+			timeMin=now,
+			timeMax=end_time,
 			singleEvents=True,
 			orderBy="startTime",
 		)
 		.execute()
 	)
-	events = events_result.get("items", [])
-	return events
+    events = events_result.get("items", [])
+    return events
 
 def get_meeting_url(event):
 	summary = event.get("summary", "No Title")
--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-from flask import Flask, render_template
 from calendar_helper import authenticate_and_get_service, find_next_meeting_url
 
-app = Flask(__name__)
-
-# Google Calendar APIの認証とサービスの取得
-service = authenticate_and_get_service()
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/next_meeting')
-def next_meeting():
-    # ここに直近5分以内の会議URLを取得するロジックを追加
+def main():
+    service = authenticate_and_get_service()
     meeting_url = find_next_meeting_url(service)
-    return render_template('next_meeting.html', meeting_url=meeting_url)
+    print(f'Next meeting URL: {meeting_url}')
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    main()
